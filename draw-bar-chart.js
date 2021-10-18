435609cfc0216b90e4e7361109bcdbbf6cde7123
Siu <siuc@protonmail.com>
2021-10-18 17:09:01 -0400

    drawing basic bars with value on top

--- draw-bar-chart.js
+++ draw-bar-chart.js
@@ -1,12 +1,17 @@
 function drawBarChart(data, options, element) {
   let chartContent = "";
-  for(let val in data) {
-    chartContent += (val + " ");
+  for(let val of data) {
+    chartContent += (`<div style="text-align: center">${val}<div style="display:flex;background-color: black; width: 20px; height: ${val}px; margin: 0px 20px"></div></div>`);
   };
-  let chart = `<div id="chart"><div id="title">TITLE</div><div id="middle"><div id="y-axis">y-axis</div><div id="chartContent">${chartContent}<div id="x-axis">x-axis</div></div></div></div>`;
+  let chart = `<div id="chart"><div id="title">TITLE</div><div id="middle"><div id="y-axis">y-axis</div><div id="right"><div id="chartContent">${chartContent}</div><div id="x-axis">x-axis</div></div></div></div>`;
   element.html(chart);
   element.css("border", "1px black solid");
+  element.css("display", "inline-block");
+  element.css("padding", "10px");
   $( "#middle" ).css( "display", "flex");
-  $( "#title" ).css( "text-align", "center");
-
+  $( "#title" ).css( "text-align", "center" );
+  $( "#chartContent" ).css("border-left", "1px black solid");
+  $( "#chartContent" ).css("border-bottom", "1px black solid");
+  $( "#chartContent" ).css("display", "flex");
+  $( "#chartContent" ).css("align-items", "flex-end");
 };
