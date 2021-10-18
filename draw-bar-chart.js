b90933176cfd67f819c10da8df4191c2b9b8554d
Siu <siuc@protonmail.com>
2021-10-18 17:24:19 -0400

    wrap the labels on x-axis

--- draw-bar-chart.js
+++ draw-bar-chart.js
@@ -1,9 +1,13 @@
 function drawBarChart(data, options, element) {
   let chartContent = "";
-  for(let val of data) {
-    chartContent += (`<div style="text-align: center">${val}<div style="display:flex;background-color: black; width: 20px; height: ${val}px; margin: 0px 20px"></div></div>`);
+  let xAxis = "";
+  for(let val of data[0]) {
+    chartContent += (`<div style="text-align: center">${val}<div style="background-color: black; width: 20px; height: ${val}px; margin: 0px 20px"></div></div>`);
   };
-  let chart = `<div id="chart"><div id="title">TITLE</div><div id="middle"><div id="y-axis">y-axis</div><div id="right"><div id="chartContent">${chartContent}</div><div id="x-axis">x-axis</div></div></div></div>`;
+  for(let val of data[1]) {
+    xAxis += (`<div style="text-align: center; overflow-wrap: break-word; width: 60px">${val}</div>`);
+  };
+  let chart = `<div id="chart"><div id="title">TITLE</div><div id="middle"><div id="y-axis">y-axis</div><div id="right"><div id="chartContent">${chartContent}</div><div id="x-axis">${xAxis}</div></div></div></div>`;
   element.html(chart);
   element.css("border", "1px black solid");
   element.css("display", "inline-block");
@@ -14,4 +18,5 @@ function drawBarChart(data, options, element) {
   $( "#chartContent" ).css("border-bottom", "1px black solid");
   $( "#chartContent" ).css("display", "flex");
   $( "#chartContent" ).css("align-items", "flex-end");
+  $( "#x-axis" ).css("display", "flex");
 };
