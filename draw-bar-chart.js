743a7f3c052734816f0912d0c3cbacf5be76d590
Siu <siuc@protonmail.com>
2021-10-18 16:49:21 -0400

    basic format
    putting title, y/x-axis, bar chart content in the right places

--- draw-bar-chart.js
+++ draw-bar-chart.js
@@ -1,5 +1,12 @@
 function drawBarChart(data, options, element) {
-  console.log("function!!!");
-  element.html( `CHANGED`);
-  console.log("function!222");
-}
+  let chartContent = "";
+  for(let val in data) {
+    chartContent += (val + " ");
+  };
+  let chart = `<div id="chart"><div id="title">TITLE</div><div id="middle"><div id="y-axis">y-axis</div><div id="chartContent">${chartContent}<div id="x-axis">x-axis</div></div></div></div>`;
+  element.html(chart);
+  element.css("border", "1px black solid");
+  $( "#middle" ).css( "display", "flex");
+  $( "#title" ).css( "text-align", "center");
+
+};
