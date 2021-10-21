fc53906f561ba26e08b4f60e4d1220ac48c5c570
Siu <siuc@protonmail.com>
2021-10-21 11:59:25 -0400

    refractor the top-level function

--- draw-bar-chart.js
+++ draw-bar-chart.js
@@ -9,6 +9,7 @@
 // for value < 1
 // make css file
 
+//function that use the options to create div for the chart title
 let makeTitleDiv = (options) => {
   let titleDiv = options.hasOwnProperty("chartTitle") ? options.chartTitle : "Untitled";
 
@@ -29,8 +30,29 @@ let makeTitleDiv = (options) => {
   return `<div class="chart-title" style="text-align: center; ${titleDiv}`;
 };
 
+// the function that find the value of tick and largest value of the tick
+let findTicks = (options, maxVal) => {
+
+  let pow;
+  for(pow = 0; maxVal > Math.pow(10, pow); pow++) {
+  }
+
+  let tickInterval = Math.pow(10, pow - 1);
+
+  tickInterval = options.hasOwnProperty("minTickVal") ? options.minTickVal : tickInterval;
+
+  let maxTick = Math.ceil(maxVal/tickInterval) * tickInterval;
+
+  return [tickInterval, maxTick];
+}
+
+// top-level function
 let drawBarChart = (data, options, element) => {
 
+  //make title Div
+  let chartTitleDiv = makeTitleDiv(options);
+
+  //for the bars
   let bar = "";
 
   //for the x-axis
@@ -40,32 +62,28 @@ let drawBarChart = (data, options, element) => {
   let yTick = "";
   let yLabel = "";
 
-  //make title Div
-  let chartTitleDiv = makeTitleDiv(options);
+  //the number of data, i.e. bar
+  let dataNum = data[0].length;
 
   //the maximum value in data
   let maxVal = Math.max(...data[0]);
 
-  //find the number of data
-  let dataNum = data[0].length;
+  //find the tick interval and value of the maximum tick
+  let [tickInterval, maxTick] = findTicks(options, maxVal);
 
-  //decide the position of ticks on y-tick
-  let pow;
-  for(pow = 0; maxVal > Math.pow(10, pow); pow++) {
-
-  }
-  let tickInterval = Math.pow(10, pow-1);
-  let maxTick = Math.ceil(maxVal/tickInterval) * tickInterval;
-  for(let i = 0; i <= maxVal; i += tickInterval) {
+  //make div for the ticks and labels on y-axis
+  for(let i = 0; i <= maxTick; i += tickInterval) {
     // minus one from tick interval to leave space for the border
-    yTick += `<div style="border-bottom: 1px black solid; width: 10px;"></div>`;
+    yTick += `<div style="border-bottom: 1px black solid"></div>`;
     yLabel += `<div style="height: 0">${i}</div>`;
   }
+
   yTick = `<div class="y-tick" id="y-tick-${options.Id}">${yTick}</div>`;
+  yLabel = `<div class="y-label" id="y-label-${options.Id}">${yLabel}</div>`;
 
   //plot the bar graph with value in parameter "data"
   for(let val of data[0]) {
-    bar += (`<div style="background-color: black; color: white; text-align: center; height: ${100 * val / maxTick}%;width: ${80/dataNum}%; margin: 0 ${10/(dataNum)}%">${val}</div>`);
+    bar += (`<div style="background-color: black; color: white; height: ${100 * val / maxTick}%;width: ${80/dataNum}%; margin: 0 ${10/(dataNum)}%">${val}</div>`);
   };
 
   //label x-axis
@@ -73,9 +91,9 @@ let drawBarChart = (data, options, element) => {
     xAxis += (`<div style="width: ${80/dataNum}%; margin: 0 ${10/dataNum}%">${val}</div>`);
   };
 
-  xAxis = `<div class="left-container"><div class="x-axis" style="text-align: center; overflow-wrap: anywhere"><div id="left-corner-${options.Id}"></div>${xAxis}</div></div>`;
+  xAxis = `<div class="x-axis"><div id="left-corner-${options.Id}"></div>${xAxis}</div>`;
 
-  let chart = `${chartTitleDiv}<div class="middle"><div class="y-label" id="y-label-${options.Id}" style="text-align: right">${yLabel}</div>${yTick}<div class="bar">${bar}</div></div>${xAxis}`;
+  let chart = `${chartTitleDiv}<div class="middle">${yLabel}${yTick}<div class="bar">${bar}</div></div>${xAxis}`;
   element.html(chart);
   element.css("height", options.height);
   element.css("width", options.width);
